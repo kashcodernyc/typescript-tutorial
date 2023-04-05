c898376b77bf0e5b99d68a05f4efa9f07d5dabcc
kashcodernyc <prakash_subba08@hotmail.com>
2023-04-05 11:39:43 -0400

    feat(tutorial): tutorial for react typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Greet } from './components/Greet';
+import { Person } from './components/Person';
+import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
+import { Container } from './components/Container';
 
 function App() {
+  const personName = {
+    first: 'Prakash',
+    last: 'Subba'
+  }
+
+  const nameList = [
+    {
+      first: 'Bruce',
+      last: 'Wayne'
+    },
+    {
+      first: 'Peter',
+      last: 'Griffin'
+    },
+    {
+      first: 'The',
+      last: 'Rock'
+    },
+
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet isLoggedIn={false} name='Prakash'/>
+      <Person name={personName}/>
+      <PersonList names = {nameList} />
+      <Status status='loading'/>
+      <Heading>Text</Heading>
+      <Oscar>
+        <Heading>oscar goes to prakash subbba</Heading>
+      </Oscar>
+      <Button handleClick={(event, id) => {
+        console.log('Button clicked', event, id);
+      }} />
+      <Input value='' handleChange={(event) => console.log(event)} />
+      <Container styles={{border: '1px solid black', padding: '1rem'}} />
     </div>
   );
 }
---
+++ src/components/state/User.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+type AuthUser = {
+    name: string
+    email: string
+}
+
+export const User = () => {
+    const [user, setUser] = useState<null | AuthUser>({} as AuthUser)
+    const handleLogin = () => {
+        setUser({
+            name: 'prakash', 
+            email: 'prakash@gmail.com',
+        })
+    }
+    
+    return (
+        <div>
+            <button onClick={handleLogin}>Login</button>
+            <div> User name is {user.name} </div>
+            <div> User email is {user.email} </div>
+        </div>
+    )
+}
